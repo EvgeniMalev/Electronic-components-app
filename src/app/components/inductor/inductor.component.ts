12a92f515d85e80c38b58ef9694f469192a6c38b
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-04-11 23:39:29 +0300

    Create inductor.component.ts

---
+++ src/app/components/inductor/inductor.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inductor',
+  templateUrl: './inductor.component.html',
+  styleUrls: ['./inductor.component.css']
+})
+export class InductorComponent {
+  value: string = '';
+  num: string = '';
+  powerRating: string = '';
+
+  addInductor() {
+   
+  }
+}
