06e3986b64aa44e3b46dc7ed176c24b2ae4ae7c3
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-04-16 22:39:20 +0300

    Create electronic-component.ts

---
+++ src/app/components/electronic-component/electronic-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-electronic-components',
+  templateUrl: './electronic-components.component.html',
+  styleUrls: ['./electronic-components.component.css'] // Linking CSS file
+})
+export class ElectronicComponentsComponent {
+  constructor() {}
+}
